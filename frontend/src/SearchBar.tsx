35863b67733c720d22426fbd242ebbc75c54b2be
Sayan Goswami <goswami.sayan47@gmail.com>
2022-02-10 03:19:42 +0530

    Updates frontend.

--- frontend/src/SearchBar.tsx
+++ frontend/src/SearchBar.tsx
@@ -7,11 +7,16 @@ import {
     Button,
     HStack,
     Input,
-    Circle,
+    Spacer,
     Image,
+    Icon,
     useToast,
-    VStack
+    SimpleGrid,
 } from "@chakra-ui/react"
+
+import {
+    StarIcon, ViewIcon
+} from "@chakra-ui/icons"
 
 const API_ENDPOINT = "http://localhost:8000/related_repositories/"
 
@@ -88,20 +93,40 @@ function SearchBar() {
             </form>
 
 
-            {listingData && listingData.map((item, index) => (
-                <Box key={index} marginTop={2} borderWidth='1px' borderRadius='lg'>
-                    <HStack>
-                        <Circle size='40px' color='white' flex="1">
-                            <Image src={item.owner_avatar_url} alt={item.owner} />
-                        </Circle>
+            <SimpleGrid minChildWidth='400px' spacing='20px'>
+                {listingData && listingData.map((item, index) => (
+                    <Box key={index} marginTop={2} borderWidth='1px' borderRadius='lg'>
+                        <HStack padding="8px">
+                            <Image
+                                src={item.owner_avatar_url}
+                                alt={item.owner}
+                                boxSize='40px'
+                                borderRadius='full'/>
 
-                        <VStack>
-                            <Text flex="1">{item.name}</Text>
-                            <Text flex="1">{item.description}</Text>
-                        </VStack>
-                    </HStack>
-                </Box>
-            ))}
+                            <Text align="start">
+                                <a href={item.url}>{item.name}</a>
+                            </Text>
+
+                            <Spacer />
+
+                            <HStack>
+                                <StarIcon />
+                                <Text>{item.stargazers_count}</Text>
+                            </HStack>
+
+                            <HStack>
+                                <ViewIcon />
+                                <Text>{item.watchers_count}</Text>
+                            </HStack>
+
+                        </HStack>
+
+                        <Box padding="8px">
+                            <Text>{item.description}</Text>
+                        </Box>
+                    </Box>
+                ))}
+            </SimpleGrid>
 
 
         </div>
